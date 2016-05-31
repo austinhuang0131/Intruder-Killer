9931f70fa586e70277c3f7976d7c08b35ddbc9f1
Austin Huang <austinhuang0131@gmail.com>
2016-05-30 20:38:57 -0400

    Updated some description
    To make this js more human-readable.

--- IntruderBot.js
+++ IntruderBot.js
@@ -1,5 +1,6 @@
 // This is the Intruder Killer, made by austinhuang#1076 in collaboration with Bingo92707#2447, Nathanael#6757, and a bunch of other people from DiscordAPI server.
 // Without doubt, this bot needs Discord.js
+// Need Rest.js too. https://github.com/cujojs/rest
 
 var Discord = require("discord.js");
 var rest = require('rest')
@@ -24,6 +25,8 @@ mybot.on("message", function(message){
         mybot.reply(message, "**Mute the intruder first**, fill our form, then execute ban. (If you have a Mee6, you can ban the intruder first.) https://goo.gl/forms/SJ86rcxgWWSkwnZk2");
     }
 });
+
+// This is the module that fetches ban list from my wiki. Thanks Snazzah#0371/SnazzyPine25.
 function hasBan(userid) {
   var list = []
   rest('http://discord.shoutwiki.com/wiki/Ban_List').then(function(response) {
@@ -36,11 +39,11 @@ function hasBan(userid) {
   });
   return list.includes(userid)
 }
-// This is the new user ID check module.
+
 mybot.on("serverNewMember", function(server, user){
   var blacklistID = ['182152242219057152','155111194183729153','138709835846909953','101018733564166144','182940650889674752','185752383500845056','185753017985925120','185753830590382091','163130206763220993','148309181806542849'];
-// Put the USER IDs you want to block here. Note that this only functions when people join.
-  if (hasBan(user.id)||blacklistID[i].includes(user.id)) {
+// For customizing your blacklist without reporting it to DSH, put IDs here.
+if (hasBan(user.id)||blacklistID[i].includes(user.id)) {
     var message = ``;
 
     message += `**Blacklisted ID found!**\n`;
