9c426812be2bfeb4149d2e95f3cb6cad31229a39
Austin Huang <austinhuang0131@gmail.com>
2016-05-31 11:58:48 -0400

    Merge test.js into main script

--- IntruderBot.js
+++ IntruderBot.js
@@ -2,24 +2,86 @@
 // Without doubt, this bot needs Discord.js
 // Need Rest.js too. https://github.com/cujojs/rest
 
-var Discord = require("discord.js");
-var rest = require('rest')
+var fs = require('fs'); // require the filesystem module
+var Discord = require("discord.js"); // Requires discord.js no doubt
+var rest = require('rest'); // Requires rest.js
 
 var mybot = new Discord.Client();
 
-mybot.on("message", function(message){
-  if (message.content === "dshset") {
-    var serverSettings.serverID = message.channel.id;
-    fs.writeFile('./servers/' + serverID + '.json', serverSettings["serverID"], function(error) {
+// Contains all settings
+var serverSettings = {};
+fs.readdir('./servers/', function(error, files) {
+  if (error) {
+    throw error; // Must be able to successfuly get the contents of the servers directory else throw an error.
+  }
+  //loop through the array "files" and read each one.
+  for (var i= 0; i < files.length; i++) {
+    (function(tmp_i) {
+      //read current json file for settings
+      fs.readFile('./servers/' + files[tmp_i] + '.json', function(error, data) {
+        if (error) {
+          console.error(error);
+          return;
+        }
+
+        // try to parse it
+        try {
+          serverSettings[files[tmp_i]] = JSON.parse(data); //Parse the settings file and add it to the global var called "serverSettings" with the key being the server ID/file name.
+        } catch(e) {
+          //throw the error if it fails. I suggest you do a bit of error handling yourself.
+          throw e;
+        }
+      });
+    })(i);
+  }
+  //login with your token
+  mybot.loginWithToken('???', function (error) {
     if (error) {
-      console.error(error);
-      return;
+      throw error;
     }
   });
-}
 });
 
-// This is the dshreport command. No need to change here.
+mybot.on('ready', function() {
+  console.info('Username: ' + mybot.user.username);
+  console.info('ID: ' + mybot.user.id);
+  console.info('Servers: ' + mybot.servers.length);
+  console.info('Channels: ' + mybot.channels.length);
+  console.info('-----------------------------------------------------------------------------');
+});
+
+mybot.on("message", function(message){
+  if (message.content === "dshset") {
+    console.log('serverSettings#1: ', serverSettings);
+    console.log('serverID: ' + message.channel.id);
+    serverSettings[message.channel.id] = {
+      "settings": {}
+    };
+    console.log('serverSettings#2: ', serverSettings);
+    serverSettings[message.channel.id].settings.whitelisted = message.channel.id; // create the key called "whitelisted" and give it the value of the channel the message was sent form
+    console.log('serverSettings[serverID]: ', serverSettings[message.channel.id]);
+	mybot.reply(message, "Success!");
+    // save the settings to a file
+    fs.writeFile('./servers/' + message.channel.id + '.json', JSON.stringify(serverSettings[message.channel.id]), function(error) {
+      if (error) {
+        console.error(error);
+        return;
+      }
+    });
+  }
+  else if (message.content === "dshremove") {
+    delete serverSettings[message.channel.id]; // remove the data from serverSettings
+    //Delete the file containing the settings
+	mybot.reply(message, "Success!");
+    fs.unlink('./servers/' + message.channel.id + '.json', function(error) {
+      if (error) {
+        console.error(error);
+        return;
+      }
+    });
+  }
+});
+
 mybot.on("message", function(message){
     if(message.content === "dshreport") {
         mybot.reply(message, "**Mute the intruder first**, fill our form, then execute ban. (If you have a Mee6, you can ban the intruder first.) https://goo.gl/forms/SJ86rcxgWWSkwnZk2");
@@ -41,17 +103,28 @@ function hasBan(userid) {
 }
 
 mybot.on("serverNewMember", function(server, user){
-  var blacklistID = ['182152242219057152','155111194183729153','138709835846909953','101018733564166144','182940650889674752','185752383500845056','185753017985925120','185753830590382091','163130206763220993','148309181806542849'];
+  var blacklistID = ['182152242219057152','155111194183729153','138709835846909953','101018733564166144','182940650889674752','185752383500845056','185753017985925120','185753830590382091','163130206763220993','148309181806542849','155784937511976960'];
 // For customizing your blacklist without reporting it to DSH, put IDs here.
-if (hasBan(user.id)||blacklistID[i].includes(user.id)) {
+if (hasBan(user.id)||blacklistID.includes(user.id)) {
     var message = ``;
 
     message += `**Blacklisted ID found!**\n`;
     message += `**User:** ${user.name} (#${user.id})\n`;
 
-    mybot.sendMessage(message.channel.id, message);
+    mybot.sendMessage(serverSettings[message.channel.id], message);
   }
 });
 
-// A token.
-mybot.loginWithToken("???");
+var interruptedAlready = false;
+process.on('SIGINT', function() {
+  if(interruptedAlready) {
+    console.log("Caught second interrupt signal... Exiting");
+    process.exit(0);
+  }
+
+  interruptedAlready = true;
+
+  console.log("Caught interrupt signal... Disconnecting");
+
+  bot.logout();
+});
