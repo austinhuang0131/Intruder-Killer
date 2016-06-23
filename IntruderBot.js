f4cf11978b5e59b8c5044e6f2162c81513666169
Austin Huang <austinhuang0131@gmail.com>
2016-06-23 07:28:48 -0400

    1.1 > 1.2

--- IntruderBot.js
+++ IntruderBot.js
@@ -2,8 +2,9 @@
 
 var rest = require('rest'); // https://github.com/cujojs/rest
 var Discord = require("discord.js"); // Requires discord.js no doubt
-var devs = ['???', '???', '???'];
-var agents = ['???', '???', '???'];
+var devs = ['???'];
+var agents = ['???'];
+var bl = ['182152242219057152', '155111194183729153', '138709835846909953', '101018733564166144', '182940650889674752', '185752383500845056', '185753017985925120', '185753830590382091', '163130206763220993', '142379865532792832', '177414298698645504', '141883348959232001', '184903632033021952', '187341009548607488', '93143850175975424', '162269375611666432', '82574907715616768', '172882961056661505', '168644375679598592', '153486961431019520', '108548330354667520', '153486961431019520', '163925197664026624', '146115953933942784', '187602068129710081', '144276155527069696', '84282077893132288', '165268334382678016', '185521195582619648', '157692268407554048', '171003901053632512', '136454060407521280', '133055234380529664', '181242643928449024', '93177065687240704', '109821286888296448', '150756167063306240', '190175506866503681', '122363757576847360', '162984946372313088', '92491108734484480', '191835100147613696', '191827268543184896', '191838891420942336', '191837919550373889', '162254357662793728', '92372944457580544', '131527476626063361', '169494230845095936', '193507398021939200', '162334579758202880', '193523277614940160', '193523277614940160', '193523277614940160', '156816025386614785', '141001087443402754', '152413443872260096', '163030561487585281', '163121606762168330', '139601973291909122', '127532575349538816', '141271486253105152', '92025602311217152', '92017896187789312', '134861419773558784', '195446123622563840'];
 
 var mybot = new Discord.Client();
 
@@ -22,7 +23,6 @@ mybot.on('ready', function() {
   console.info('Channels: ' + mybot.channels.length);
   console.info('---------------------------');
   mybot.setPlayingGame('http://bit.ly/discserv');
-  mybot.sendMessage('188796780597477376', "Konichiwa! Intruder Killer is here!");
 });
 
 // ping by Snazzah
@@ -52,7 +52,7 @@ mybot.on("message", function(message,server,channel){
   }
   else if (message.content === "dshkill") {
 	if(devs.includes(message.author.id)){
-		mybot.sendMessage(message.id, "Why...Why? I feel like I'm lying in the blood...Is this...death? Where...Where am...I...?");
+		mybot.sendMessage(message.id, "Why...Why? I feel like I'm lying in the blood...Is this...death? Where...Where am...I...Sayonara...");
 		console.info("Restart requested by "+message.author.name);
 		setTimeout(function() { mybot.logout(); }, 500);
 	}else{
@@ -114,6 +114,8 @@ mybot.on("message", function(message,server,channel){
 		      mybot.reply(message, "Here's my hammer! Use it carefully!");
       }
 	 });
+	} else {
+	  mybot.reply(message, "Want my hammer? How about swinging my hammer at...you?");	
 	}
   }
   else if (message.content === "dshctrlend") {
@@ -121,40 +123,68 @@ mybot.on("message", function(message,server,channel){
 	    console.info("Hammer returned from "+message.author.name);
    	  mybot.deleteRole(message.server.roles.get("name", "DSH"));
 		  mybot.setStatusOnline();
-		  mybot.reply(message, "Thanks for returning my hammer!");
+		  mybot.reply(message, "Thanks for returning my hammer! Arigato!");
 	}   
   }
 });
 
-// Autoban by Snazzah
-function hasBan(userid) {
-    var list = [];
+//function hasBan(userid) {
+//    var list = [];
+//    rest('http://discord.shoutwiki.com/wiki/Ban_List').then(function(response) {
+//      var templist = response.entity.split('<table class="wikitable">')[1].split('<td>').join('').split('</td>');
+//      templist.forEach(function(item){
+//        if(!isNaN(Number(item))){
+//          list.push(String(Number(item)));
+//        }
+//      });
+//    });
+//    setTimeout(function() {
+//      if(list.length === 0){
+//        return hasBan(userid);
+//      }else{
+//        return list.includes(userid);
+//      }
+//    }, 500);
+//}
+
+mybot.on("serverNewMember", function(server, user){
+	var list = [];
     rest('http://discord.shoutwiki.com/wiki/Ban_List').then(function(response) {
-      var templist = response.entity.split('<table class="wikitable">')[1].split('<td>').join('').split('</td>');
-      templist.forEach(function(item, index){
+    var templist = response.entity.split('<table class="wikitable">')[1].split('<td>').join('').split('</td>');
+      templist.forEach(function(item){
         if(!isNaN(Number(item))){
           list.push(String(Number(item)));
         }
       });
     });
     setTimeout(function() {
-      if(list.length == 0){
-        return hasBan(userid);
-      }else{
-        return list.includes(userid);
+      if(list.length === 0){
+        if(bl.includes(user.id)) {
+        console.info('Seems like I found a bad guy named '+user.name+'! ('+user.id+')');
+        mybot.banMember(user,server,7,function(error){
+          if(error){
+            mybot.sendMessage('Wait, what...I got **an error** when swinging the ban hammer? Did I have **ban members** permission?');
+            console.info('Ban failed on '+server.name);
+          } else {
+            mybot.sendMessage(server, "I cannot wait for the hammer to come out...Idekimasuyo!");
+  	        console.info('And he is banned from that server!');
+	        }
+        });
+        }
+      }
+      else
+      return list.includes(user.id);
+      if (list.includes(user.id)) {
+        console.info('Seems like I found a bad guy named '+user.name+'! ('+user.id+')');
+        mybot.banMember(user,server,7,function(error){
+          if(error){
+            mybot.sendMessage('Wait, what...I got **an error** when swinging the ban hammer? Did I have **ban members** permission?');
+            console.info('Ban failed on '+server.name);
+          } else {
+            mybot.sendMessage(server, "I cannot wait for the hammer to come out...Idekimasuyo!");
+  	        console.info('And he is banned from '+server.name+'!');
+	        }
+        });
       }
     }, 500);
-}
-
-mybot.on("serverNewMember", function(server, user){
-  if (hasBan(user.id)) {
-    console.info('Seems like I found a bad guy named '+user.name+'! ('+user.id+')');
-	mybot.sendMessage(server, "Mhahahaha Nasty creatures...GET BANNED!!!");
-    server.banmember(user,server,function(error){
-      if(error){
-        mybot.sendMessage('Wait, what...I got **an error** when swinging the ban hammer? Did I have **ban members** permission?');
-        console.info('Damn Daniel, there is an error when swinging the ban hammer!');
-      }
-    });
-  }
 });
